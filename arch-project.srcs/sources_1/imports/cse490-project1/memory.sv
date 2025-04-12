88299960a8ddf630f69f8d76d25c959ba9635294
nfox18212 <nfox18212@gmail.com>
2025-04-12 12:36:21 -0400

    separated data memory and instruction memory into two modules

--- arch-project.srcs/sources_1/imports/cse490-project1/memory.sv
+++ arch-project.srcs/sources_1/imports/cse490-project1/memory.sv
@@ -1,14 +1,47 @@
 `include "defines.sv"
 
-module instruction_memory(
-    input reg clock, // read or write
+
+module data_memory(
     input reg [15:0] inWord, // word to write
     input int addr, optype, // memory address to access
     output reg [15:0] outWord // read word
 );
 
-    reg [7:0] instruction_mem [0:63] =  '{ 'h01, 'h20, 'h30, 'h35, 'h60, 'h05, 'h31, 'h40, 'h00, 'h21, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00 };
     reg [7:0] data_mem [64:127] = '{ default: 'h0 };
+    always_comb begin
+//        $readmemh("memory.mem", memoryArray, 0, 127); // initialize memory into array
+//        $stop();
+
+        if (optype == `READ) begin
+    
+          outWord[7:0]  = data_mem[addr+1]; // don't need to validate reading, read lower word
+          outWord[15:8] = data_mem[addr];
+          `ifdef dbg
+            $display("[debug:memory] outWord = %4h", outWord);
+          `endif
+        end     
+        
+        if (optype == `WRITE) begin
+
+
+              data_mem[addr] = inWord[7:0];
+              data_mem[addr+1] = inWord[15:8]; // write upper word
+
+              $writememh("memory.mem", data_mem, 64, 127); // can only write entire contents of memory, not individual bytes
+              // return input word as output
+              outWord = inWord;
+        end
+
+    end
+endmodule
+
+
+module instruction_memory(
+    input int addr, optype, // memory address to access
+    output reg [15:0] outWord // read word
+);
+
+    reg [7:0] instruction_mem [0:63] =  '{ 'h01, 'h20, 'h30, 'h35, 'h60, 'h05, 'h31, 'h40, 'h00, 'h21, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00, 'h00 };
 
     `ifdef dbg
     reg [7:0] temp1 ='h0;
@@ -18,45 +51,17 @@ module instruction_memory(
     always_comb begin
 //        $readmemh("memory.mem", memoryArray, 0, 127); // initialize memory into array
 //        $stop();
-        if (optype == `READ) begin
+        
           `ifdef dbg
-          $display("[debug:memory] operation is READ, time=%1d", $time);
-          temp1 = instruction_mem[addr];
-          temp2 = instruction_mem[addr+1];
+            $display("[debug:memory] operation is READ, time=%1d", $time);
+            temp1 = instruction_mem[addr];
+            temp2 = instruction_mem[addr+1];
           `endif
+          
           outWord[7:0]  = instruction_mem[addr+1]; // don't need to validate reading, read lower word
           outWord[15:8] = instruction_mem[addr];
-          `ifdef dbg
-            $display("[debug:memory] outWord = %4h", outWord);
-          `endif
-        end 
-        
-        if (optype == `WRITE) begin
-            // ASSUMING WRITE
-            $display("[debug:memory] operation is WRITE");
-            // it makes sense to nest here
-            if (addr > 'h3F) begin // write operations are only valid from 0x40 to 0x7F, 0x0 to 0x3F are read-only
-              instruction_mem[addr] = inWord[7:0];
-              instruction_mem[addr+1] = inWord[15:8]; // write upper word
-              `ifdef dbg
-
-              temp1 = instruction_mem[addr];
-              temp2 = instruction_mem[addr+1];
-              $display("[debug:memory] addr=0x%2h, addr+1=0x%2h, temp1=0x%2h, temp2=0x%2h", addr, addr+1, temp1, temp2);
-              `endif
-              $writememh("memory.mem", instruction_mem, 0, 127); // can only write entire contents of memory, not individual bytes
-              // return input word as output
-              outWord = inWord;
-            end else begin
-              // if addr is not more than 0x3F then we're trying to write to
-              // read-only memory, return null vector
-              outWord = 16'h0; 
-            end
-
-        end
 
     end
-
 
 
 endmodule
