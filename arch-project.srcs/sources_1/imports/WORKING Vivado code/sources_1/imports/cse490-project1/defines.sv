0a9b42d0d16d4603ace3778be66d6934ccfa6f65
osmonduu <110065770+osmonduu@users.noreply.github.com>
2025-09-29 19:08:24 -0400

    Removed vivado project artifacts. Retained SV source files

--- arch-project.srcs/sources_1/imports/WORKING Vivado code/sources_1/imports/cse490-project1/defines.sv
+++
@@ -1,5 +0,0 @@
-`define READ 0
-`define WRITE 1
-`define PCR 16 // program counter index
-`define tick #1clock=1;#1clock=0
-`define dbg
--- arch-project.srcs/sources_1/imports/cse490-project1/test-program.s
+++
@@ -1,4 +0,0 @@
-add   r1, r2, r1
-add   r0, r0, r1
-addi  r0, r3, 5
-
